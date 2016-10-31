6a909fb11605c21a4b80f29283198c25172d54a0
Varghese Paul <varkychen@gmail.com>
2016-10-31 15:26:22 +0530

    Fixing code style

--- src/hackerrank/algos/ArrayLeftRotation.java
+++ src/hackerrank/algos/ArrayLeftRotation.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class ArrayLeftRotation {
-    
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
         int a[] = new int[n];
-        
-        for(int i=0; i < n; i++){
+
+        for (int i = 0; i < n; i++) {
             a[i] = in.nextInt();
         }
         in.close();
-        
+
         int[] newA = new int[n];
-        IntStream.range(0, n).forEach(i -> newA[(i - k + n)%n] = a[i]);
+        IntStream.range(0, n).forEach(i -> newA[(i - k + n) % n] = a[i]);
         Arrays.stream(newA).forEach(i -> System.out.print(i + " "));
     }
 }
